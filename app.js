a94b90565a79568db4bc1d90d47b397a2c83f577
Chris Michels <chrisjohnmichels@gmail.com>
2019-02-10 21:06:38 -0500

    fix

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('*', (request, response) => {
+   response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
